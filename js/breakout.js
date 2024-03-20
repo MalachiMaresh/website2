26a08fed798d57618c1069c11768c022670a562f
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-03-20 17:09:52 +0000

    Wed, Mar 20, 2024, 10:09 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -43,6 +43,10 @@ brickInfo = {
 bricks = []
 for (let i = 0; i < brickRowCount; i++) {
     bricks[i] = []
+    for (let j = 0; j < brickColumnCount; j++){
+        let x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+
+    }
 }
 
 // Draw ball on canvas

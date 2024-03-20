f5800461f93d43f8bb56ee22ffb0785329ddc710
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-03-20 17:13:09 +0000

    Wed, Mar 20, 2024, 10:13 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -44,7 +44,9 @@ bricks = []
 for (let i = 0; i < brickRowCount; i++) {
     bricks[i] = []
     for (let j = 0; j < brickColumnCount; j++){
-        let x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
+        bricks[i][j] = 
 
     }
 }

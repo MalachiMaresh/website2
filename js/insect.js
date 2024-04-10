b9540cc05f4d1e341eb905e4bb15a0baee0e1154
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-04-10 16:41:17 +0000

    Wed, Apr 10, 2024, 9:41 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -35,6 +35,12 @@ function startGame() {
 function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
+    if (m < 10) {
+        m = `0${m}`
+    }
+    if (s < 10) {
+        s = `0${s}`
+    }
 }
 
 function createInsect() {

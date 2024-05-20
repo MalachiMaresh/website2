35ccf52404e5aceaaa14a13fdcd6889815881afc
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-05-20 02:14:03 +0000

    Sun, May 19, 2024, 7:14 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-btn = document.querySelector('.btn')
+btn1 = document.querySelector('.btn1')
+btn2 = document.querySelector('.btn2')
+btn3 = document.querySelector('.btn3')
 search = document.querySelector('.search')
 
 btn.addEventListener('click', () => {

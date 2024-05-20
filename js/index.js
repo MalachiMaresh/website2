98ea1318d85fd23a8bd7e59840a9165fefbbe5e4
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-05-20 02:18:04 +0000

    Sun, May 19, 2024, 7:18 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 btn1 = document.querySelector('.btn1')
 btn2 = document.querySelector('.btn2')
 btn3 = document.querySelector('.btn3')
-search = document.querySelector('.search')
+ins = document.querySelector('.ins')
+br = document.querySelector('.br')
+ha = document.querySelector('.ha')
 
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
+btn1.addEventListener('click', () => {
+    ins.classList.toggle('active')
 })
+
+
+btn2.addEventListener('click', () => {
+    br.classList.toggle('active')
+})
+
+
+btn3.addEventListener('click', () => {
+    ha.classList.toggle('active')
+})
+
